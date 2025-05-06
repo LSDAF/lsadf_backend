2966a5e9c213241f924ef39033ed20509370f9f3
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-05-06 18:26:37 +0200

    chore: rollback + Simplify docker build script by removing unused targets.
    Removed deprecated build targets for Keycloak, RedisInsight, and API build via GitHub Container Registry (GHCR). Streamlined the makefile to focus on active and required build processes, improving maintainability.

--- .make/docker-build.mk
+++ .make/docker-build.mk
@@ -4,25 +4,11 @@ build:
 build-api: install
 	COMPOSE_PROFILES=backend docker-compose --env-file env/env.properties -f dc-local.yml build
 
-build-keycloak:
-	docker pull quay.io/keycloak/keycloak:26.0.0 --platform linux/arm64
-	docker tag quay.io/keycloak/keycloak:26.0.0 quay.io/keycloak/keycloak:26.0.0-arm64
-
-build-redisinsight:
-	docker pull redis/redisinsight:2.66 --platform linux/arm64
-	docker tag redis/redisinsight:2.66 redis/redisinsight:2.66-arm64
-	minikube image load redis/redisinsight:2.66-arm64
-
 build-api-dev: install
-	#COMPOSE_PROFILES=backend_dev docker-compose --env-file env/env.properties -f dc-local.yml build
-	docker build -t lsadf/lsadf-api-dev-arm64:latest -f docker/Dockerfile-dev . --build-arg module_folder=lsadf_api --platform=linux/arm64/v8
-	minikube image load lsadf/lsadf-api-dev-arm64:latest
+	COMPOSE_PROFILES=backend_dev docker-compose --env-file env/env.properties -f dc-local.yml build
 
 build-admin: install-docker admin-install
 	COMPOSE_PROFILES=backend_admin docker-compose --env-file env/env.properties -f dc-local.yml build --build-arg BRANCH_NAME=$BRANCH_NAME
 
 build-admin-dev: install-docker admin-install
 	COMPOSE_PROFILES=backend_admin_dev docker-compose --env-file env/env.properties -f dc-local.yml build
-
-build-api-ghcr: install-no-env
-	docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/lsdaf/lsadf_api:latest -f docker/Dockerfile . --build-arg module_folder=lsadf_api
