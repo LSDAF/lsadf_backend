92d50ef41366fd972eaa561c6badd9e827ffb446
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-05-28 23:28:35 +0200

    feat: Remove JwtAuthentication class and update import references
    The JwtAuthentication class was removed from JsonAttributes, and related references were updated to use the correct location. This improves code organization and aligns import usage with the appropriate package.

--- lsadf_core/src/main/java/com/lsadf/core/infra/web/JsonAttributes.java
+++ lsadf_core/src/main/java/com/lsadf/core/infra/web/JsonAttributes.java
@@ -104,12 +104,4 @@ public final class JsonAttributes {
     public static final String STATISTIC = "statistic";
     public static final String BASE_VALUE = "base_value";
   }
-
-  @NoArgsConstructor(access = AccessLevel.PRIVATE)
-  public static final class JwtAuthentication {
-    public static final String ACCESS_TOKEN = "access_token";
-    public static final String REFRESH_TOKEN = "refresh_token";
-    public static final String EXPIRES_IN = "expires_in";
-    public static final String REFRESH_EXPIRES_IN = "refresh_expires_in";
-  }
 }
