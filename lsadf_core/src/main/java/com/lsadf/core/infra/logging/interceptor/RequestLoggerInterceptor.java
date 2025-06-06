cf6b28d238aef12965b840d42a8c79da68dbfc61
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-06-06 19:35:19 +0200

    feat: Rename RequestLoggerInterceptor package for consistency
    Updated the package name from `interceptors` to `interceptor` to align with naming conventions. Adjusted all related imports accordingly to maintain functionality.

--- lsadf_core/src/main/java/com/lsadf/core/infra/logging/interceptor/RequestLoggerInterceptor.java
+++ lsadf_core/src/main/java/com/lsadf/core/infra/logging/interceptor/RequestLoggerInterceptor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.lsadf.core.infra.logging.interceptors;
+package com.lsadf.core.infra.logging.interceptor;
 
 import static com.lsadf.core.infra.logging.LogColor.*;
 
