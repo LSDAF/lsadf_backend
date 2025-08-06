7657b05435411e4438fee111627136065142e987
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-08-06 23:57:25 +0200

    feat: Created new beans for new adapters

--- lsadf_core/src/main/java/com/lsadf/core/infra/valkey/cache/config/ValkeyCacheRepositoryConfiguration.java
+++ lsadf_core/src/main/java/com/lsadf/core/infra/valkey/cache/config/ValkeyCacheRepositoryConfiguration.java
@@ -16,9 +16,57 @@
 
 package com.lsadf.core.infra.valkey.cache.config;
 
+import com.lsadf.core.application.game.save.characteristics.CharacteristicsCachePort;
+import com.lsadf.core.application.game.save.currency.CurrencyCachePort;
+import com.lsadf.core.application.game.save.metadata.GameMetadataCachePort;
+import com.lsadf.core.application.game.save.stage.StageCachePort;
+import com.lsadf.core.infra.valkey.cache.config.properties.CacheExpirationProperties;
+import com.lsadf.core.infra.valkey.cache.game.save.characteristics.CharacteristicsCacheRepositoryAdapter;
+import com.lsadf.core.infra.valkey.cache.game.save.characteristics.CharacteristicsHashRepository;
+import com.lsadf.core.infra.valkey.cache.game.save.currency.CurrencyCacheRepositoryAdapter;
+import com.lsadf.core.infra.valkey.cache.game.save.currency.CurrencyHashRepository;
+import com.lsadf.core.infra.valkey.cache.game.save.metadata.GameMetadataCacheRepositoryAdapter;
+import com.lsadf.core.infra.valkey.cache.game.save.metadata.GameMetadataHashRepository;
+import com.lsadf.core.infra.valkey.cache.game.save.stage.StageCacheRepositoryAdapter;
+import com.lsadf.core.infra.valkey.cache.game.save.stage.StageHashRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
-@EnableRedisRepositories(basePackages = "com.lsadf.core.infra.valkey.cache")
-public class ValkeyCacheRepositoryConfiguration {}
+@EnableRedisRepositories(
+    basePackages = "com.lsadf.core.infra.valkey.cache",
+    shadowCopy = RedisKeyValueAdapter.ShadowCopy.ON,
+    enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_DEMAND)
+public class ValkeyCacheRepositoryConfiguration {
+  @Bean
+  public CharacteristicsCachePort characteristicsCachePort(
+      CharacteristicsHashRepository characteristicsHashRepository,
+      CacheExpirationProperties cacheExpirationProperties) {
+    return new CharacteristicsCacheRepositoryAdapter(
+        characteristicsHashRepository, cacheExpirationProperties);
+  }
+
+  @Bean
+  public CurrencyCachePort currencyCachePort(
+      CurrencyHashRepository currencyHashRepository,
+      CacheExpirationProperties cacheExpirationProperties) {
+    return new CurrencyCacheRepositoryAdapter(currencyHashRepository, cacheExpirationProperties);
+  }
+
+  @Bean
+  public GameMetadataCachePort gameMetadataCachePort(
+      GameMetadataHashRepository gameMetadataHashRepository,
+      CacheExpirationProperties cacheExpirationProperties) {
+    return new GameMetadataCacheRepositoryAdapter(
+        gameMetadataHashRepository, cacheExpirationProperties);
+  }
+
+  @Bean
+  public StageCachePort stageCachePort(
+      StageHashRepository stageHashRepository,
+      CacheExpirationProperties cacheExpirationProperties) {
+    return new StageCacheRepositoryAdapter(stageHashRepository, cacheExpirationProperties);
+  }
+}
