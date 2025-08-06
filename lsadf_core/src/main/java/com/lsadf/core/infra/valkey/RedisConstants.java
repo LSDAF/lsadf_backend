0d01fc5b7ec5e70278a9805052cb3cbf6aed26bf
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-08-06 13:42:52 +0200

    chore: updating RedisConstants

--- lsadf_core/src/main/java/com/lsadf/core/infra/valkey/RedisConstants.java
+++ lsadf_core/src/main/java/com/lsadf/core/infra/valkey/RedisConstants.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.lsadf.core.infra.cache;
+package com.lsadf.core.infra.valkey;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -25,10 +25,8 @@ public final class RedisConstants {
   public static final String CHARACTERISTICS = "characteristics:";
   public static final String CURRENCY = "currency:";
   public static final String CURRENCY_HISTO = "currency_histo:";
-  public static final String INVENTORY = "inventory:";
-  public static final String INVENTORY_HISTO = "inventory_histo:";
   public static final String STAGE = "stage:";
   public static final String STAGE_HISTO = "stage_histo:";
-  public static final String GAME_SAVE_OWNERSHIP = "game_save_ownership:";
+  public static final String GAME_METADATA = "game_metadata:";
   public static final String INVALIDATED_JWT_TOKEN = "invalidated_jwt_token:";
 }
