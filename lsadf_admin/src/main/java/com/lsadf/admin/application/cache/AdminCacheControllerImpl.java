321bd4d3d7cad4d09966816c7fba2cfb78196d38
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-08-31 17:00:31 +0200

    fix: update cache flush logic in AdminCacheControllerImpl
    - Replaced multiple cache flush calls with `flushGameSaves` to streamline cache management.
    - Removed redundant calls to characteristics, currencies, stages, and redis cache flush methods.

--- lsadf_admin/src/main/java/com/lsadf/admin/application/cache/AdminCacheControllerImpl.java
+++ lsadf_admin/src/main/java/com/lsadf/admin/application/cache/AdminCacheControllerImpl.java
@@ -69,11 +69,7 @@ public class AdminCacheControllerImpl extends BaseController implements AdminCac
     validateUser(jwt);
 
     log.info("Clearing all caches");
-    cacheFlushService.flushCharacteristics();
-    cacheFlushService.flushCurrencies();
-    cacheFlushService.flushStages();
-    redisCacheManager.clearCaches();
-
+    cacheFlushService.flushGameSaves();
     return generateResponse(HttpStatus.OK);
   }
 }
