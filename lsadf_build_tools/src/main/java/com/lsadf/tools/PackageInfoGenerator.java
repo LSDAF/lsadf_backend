2ecc37f53fd8d00dcfad635d5de0d5bcd89ffeb2
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-08-31 17:19:05 +0200

    feat: add license header to PackageInfoGenerator output
    - Introduced a `LICENSE_HEADER` constant for Apache License 2.0.
    - Updated `PackageInfoGenerator` to include the license header in generated `package-info.java` files.
    - Ensures compliance with licensing requirements in auto-generated code.

--- lsadf_build_tools/src/main/java/com/lsadf/tools/PackageInfoGenerator.java
+++ lsadf_build_tools/src/main/java/com/lsadf/tools/PackageInfoGenerator.java
@@ -28,6 +28,26 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PackageInfoGenerator {
 
+  private static final String LICENSE_HEADER =
+      """
+      /*
+       * Copyright © 2024-2025 LSDAF
+       *
+       * Licensed under the Apache License, Version 2.0 (the "License");
+       * you may not use this file except in compliance with the License.
+       * You may obtain a copy of the License at
+       *
+       *     http://www.apache.org/licenses/LICENSE-2.0
+       *
+       * Unless required by applicable law or agreed to in writing, software
+       * distributed under the License is distributed on an "AS IS" BASIS,
+       * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+       * See the License for the specific language governing permissions and
+       * limitations under the License.
+       */
+
+      """;
+
   private static final String PACKAGE_INFO_JAVA = "package-info.java";
   private static final String JAVA_EXTENSION = ".java";
 
@@ -58,11 +78,12 @@ public class PackageInfoGenerator {
 
                       Path targetFile = targetDir.resolve(PACKAGE_INFO_JAVA);
                       String content =
-                          """
+                          LICENSE_HEADER
+                              + """
                                     @org.jspecify.annotations.NullMarked
                                     package %s;
                                     """
-                              .formatted(pkgName);
+                                  .formatted(pkgName);
 
                       Files.writeString(targetFile, content);
                       log.info("Generated package-info.java for {}", pkgName);
