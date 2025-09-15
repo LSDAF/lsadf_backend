94e753c82688b0b381ba8f32b6f71b02d1f20094
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-09-15 20:34:20 +0200

    feat: creating all interfaces for game session application service

---
+++ lsadf_core/src/main/java/com/lsadf/core/application/game/session/GameSessionQueryService.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2024-2025 LSDAF
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lsadf.core.application.game.session;
+
+import com.lsadf.core.domain.game.session.GameSession;
+import java.util.List;
+import java.util.UUID;
+
+public interface GameSessionQueryService {
+  GameSession findGameSessionById(UUID id);
+
+  List<GameSession> findGameSessionByGameSaveId(UUID gameSaveId);
+}
