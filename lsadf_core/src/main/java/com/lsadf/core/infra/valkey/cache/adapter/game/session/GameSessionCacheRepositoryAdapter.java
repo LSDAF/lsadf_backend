6dfa0c188aca38526d863036e0edd7f3c7a4baf8
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-09-15 20:43:39 +0200

    feat: completing all required classes in cache infra package

---
+++ lsadf_core/src/main/java/com/lsadf/core/infra/valkey/cache/adapter/game/session/GameSessionCacheRepositoryAdapter.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright © 2024-2025 LSDAF
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lsadf.core.infra.valkey.cache.adapter.game.session;
+
+import com.lsadf.core.application.game.session.GameSessionCachePort;
+import com.lsadf.core.domain.game.session.GameSession;
+import com.lsadf.core.infra.valkey.cache.HashModelMapper;
+import com.lsadf.core.infra.valkey.cache.HashRepository;
+import com.lsadf.core.infra.valkey.cache.adapter.ValkeyCacheRepositoryAdapter;
+import com.lsadf.core.infra.valkey.cache.game.session.GameSessionHash;
+import com.lsadf.core.infra.valkey.cache.game.session.GameSessionHashMapper;
+import com.lsadf.core.infra.valkey.config.properties.ValkeyCacheExpirationProperties;
+import java.util.Optional;
+import java.util.UUID;
+
+public class GameSessionCacheRepositoryAdapter
+    extends ValkeyCacheRepositoryAdapter<GameSession, GameSessionHash, UUID>
+    implements GameSessionCachePort {
+
+  private static final HashModelMapper<GameSessionHash, GameSession> HASH_MAPPER =
+      GameSessionHashMapper.INSTANCE;
+
+  public GameSessionCacheRepositoryAdapter(
+      HashRepository<GameSessionHash, UUID> repository,
+      ValkeyCacheExpirationProperties valkeyCacheExpirationProperties) {
+    super(repository);
+    this.hashMapper = HASH_MAPPER;
+    this.expirationSeconds = valkeyCacheExpirationProperties.getGameSessionExpirationSeconds();
+  }
+
+  @Override
+  public Optional<GameSession> get(String key) {
+    UUID uuid = UUID.fromString(key);
+    Optional<GameSessionHash> optional = repository.findById(uuid);
+    return optional.map(hashMapper::map);
+  }
+
+  @Override
+  public void set(String key, GameSession value) {
+    UUID uuid = UUID.fromString(key);
+    GameSessionHash hash =
+        GameSessionHash.builder()
+            .id(uuid)
+            .expiration(this.expirationSeconds)
+            .gameSaveId(value.getGameSaveId())
+            .endTime(value.getEndTime())
+            .userEmail(value.getUserEmail())
+            .build();
+
+    repository.save(hash);
+  }
+
+  @Override
+  public void unset(String key) {
+    UUID uuid = UUID.fromString(key);
+    repository.deleteById(uuid);
+  }
+}
---
+++ lsadf_core/src/main/java/com/lsadf/core/infra/valkey/cache/game/session/GameSessionHash.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright © 2024-2025 LSDAF
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lsadf.core.infra.valkey.cache.game.session;
+
+import static com.lsadf.core.infra.valkey.cache.game.session.GameSessionHash.GameSessionHashAttributes.*;
+import static com.lsadf.core.infra.valkey.cache.game.session.GameSessionHash.GameSessionHashAttributes.GAME_SESSION_GAME_SAVE_ID;
+
+import com.lsadf.core.infra.valkey.cache.Hash;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.relational.core.mapping.Column;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash(GAME_SESSION_HASH_KEY)
+@Getter
+public class GameSessionHash implements Hash<UUID> {
+  @Id
+  @Column(value = GAME_SESSION_ID)
+  private UUID id;
+
+  @Column(value = GAME_SESSION_GAME_SAVE_ID)
+  private UUID gameSaveId;
+
+  @Column(value = GAME_SESSION_USER_EMAIL)
+  private String userEmail;
+
+  @Column(value = GAME_SESSION_END_TIME)
+  private Instant endTime;
+
+  @TimeToLive private Long expiration;
+
+  @Override
+  public UUID getId() {
+    return id;
+  }
+
+  @NoArgsConstructor(access = AccessLevel.PRIVATE)
+  public static class GameSessionHashAttributes {
+    public static final String GAME_SESSION_ID = "id";
+    public static final String GAME_SESSION_GAME_SAVE_ID = "gameSaveId";
+    public static final String GAME_SESSION_USER_EMAIL = "userEmail";
+    public static final String GAME_SESSION_END_TIME = "endTime";
+    public static final String GAME_SESSION_HASH_KEY = "game_session";
+  }
+}
