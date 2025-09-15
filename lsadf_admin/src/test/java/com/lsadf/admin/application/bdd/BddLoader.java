044364d5c5dfb1458a24f9416b92a8eb8824a27a
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-09-15 20:32:13 +0200

    chore: added autowired repositories into Admin BddLoader

--- lsadf_admin/src/test/java/com/lsadf/admin/application/bdd/BddLoader.java
+++ lsadf_admin/src/test/java/com/lsadf/admin/application/bdd/BddLoader.java
@@ -58,7 +58,9 @@ import com.lsadf.core.infra.persistence.impl.game.save.characteristics.Character
 import com.lsadf.core.infra.persistence.impl.game.save.currency.CurrencyRepository;
 import com.lsadf.core.infra.persistence.impl.game.save.metadata.GameMetadataRepository;
 import com.lsadf.core.infra.persistence.impl.game.save.stage.StageRepository;
+import com.lsadf.core.infra.persistence.impl.game.session.GameSessionRepository;
 import com.lsadf.core.infra.persistence.impl.view.GameSaveViewRepository;
+import com.lsadf.core.infra.persistence.impl.view.GameSessionViewRepository;
 import com.lsadf.core.infra.valkey.cache.flush.CacheFlushService;
 import com.lsadf.core.infra.valkey.config.properties.ValkeyCacheExpirationProperties;
 import com.lsadf.core.infra.web.config.keycloak.properties.KeycloakProperties;
@@ -157,6 +159,10 @@ public class BddLoader {
 
   // Repositories
   @Autowired protected CharacteristicsRepository characteristicsRepository;
+
+  @Autowired protected GameSessionRepository gameSessionRepository;
+
+  @Autowired protected GameSessionViewRepository gameSessionViewRepository;
 
   @Autowired protected CurrencyRepository currencyRepository;
 
