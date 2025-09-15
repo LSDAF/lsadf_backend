dadc461f6cfa26ef13fcba4cc4b0b2bfc424ffd0
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-09-15 20:35:17 +0200

    feat: creating new implementations of query and command services

---
+++ lsadf_core/src/main/java/com/lsadf/core/application/game/session/impl/GameSessionCommandServiceImpl.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2024-2025 LSDAF
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lsadf.core.application.game.session.impl;
+
+import com.lsadf.core.application.cache.CacheManager;
+import com.lsadf.core.application.clock.ClockService;
+import com.lsadf.core.application.game.session.GameSessionCachePort;
+import com.lsadf.core.application.game.session.GameSessionCommandService;
+import com.lsadf.core.application.game.session.GameSessionRepositoryPort;
+import com.lsadf.core.application.game.session.command.InitializeSessionCommand;
+import com.lsadf.core.application.game.session.command.UpdateSessionEndTimeCommand;
+import com.lsadf.core.domain.game.session.GameSession;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class GameSessionCommandServiceImpl implements GameSessionCommandService {
+
+  private final GameSessionRepositoryPort gameSessionRepositoryPort;
+  private final CacheManager cacheManager;
+  private final GameSessionCachePort gameSessionCachePort;
+  private final ClockService clockService;
+
+  @Override
+  public GameSession initializeGameSession(InitializeSessionCommand command) {
+    Instant instant = clockService.getClock().instant();
+    Instant added = instant.plus(15, ChronoUnit.MINUTES);
+    GameSession gameSession =
+        gameSessionRepositoryPort.createNewGameSession(command.gameSaveId(), added);
+    if (Boolean.TRUE.equals(cacheManager.isEnabled())) {
+      gameSessionCachePort.set(gameSession.getId().toString(), gameSession);
+    }
+    return gameSession;
+  }
+
+  @Override
+  public GameSession updateGameSessionEndTime(UpdateSessionEndTimeCommand command) {
+    Instant instant = clockService.getClock().instant();
+    Instant added = instant.plus(15, ChronoUnit.MINUTES);
+    GameSession updated =
+        gameSessionRepositoryPort.updateGameSessionEndTime(command.gameSaveId(), added);
+    if (Boolean.TRUE.equals(cacheManager.isEnabled())) {
+      gameSessionCachePort.set(updated.getId().toString(), updated);
+    }
+    return updated;
+  }
+}
---
+++ lsadf_core/src/main/java/com/lsadf/core/application/game/session/impl/GameSessionQueryServiceImpl.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright © 2024-2025 LSDAF
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lsadf.core.application.game.session.impl;
+
+import com.lsadf.core.application.cache.CacheManager;
+import com.lsadf.core.application.game.session.GameSessionCachePort;
+import com.lsadf.core.application.game.session.GameSessionQueryService;
+import com.lsadf.core.application.game.session.GameSessionRepositoryPort;
+import com.lsadf.core.domain.game.session.GameSession;
+import com.lsadf.core.exception.http.NotFoundException;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class GameSessionQueryServiceImpl implements GameSessionQueryService {
+
+  private final GameSessionRepositoryPort gameSessionRepositoryPort;
+
+  private final GameSessionCachePort gameSessionCachePort;
+
+  private final CacheManager cacheManager;
+
+  @Override
+  public GameSession findGameSessionById(UUID id) {
+    if (Boolean.TRUE.equals(cacheManager.isEnabled())) {
+      var optional = gameSessionCachePort.get(id.toString());
+      if (optional.isPresent()) {
+        return optional.get();
+      }
+      var gameSession = getGameSessionFromDatabase(id);
+      gameSessionCachePort.set(id.toString(), gameSession);
+      return gameSession;
+    }
+    return getGameSessionFromDatabase(id);
+  }
+
+  @Override
+  public List<GameSession> findGameSessionByGameSaveId(UUID gameSaveId) {
+    return gameSessionRepositoryPort.getGameSessionsByGameSaveId(gameSaveId);
+  }
+
+  private GameSession getGameSessionFromDatabase(UUID id) {
+    return gameSessionRepositoryPort
+        .getGameSessionById(id)
+        .orElseThrow(() -> new NotFoundException("Didn't find any game session with id " + id));
+  }
+}
