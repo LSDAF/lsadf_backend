517899a542921987aad573db77fe21934d9d47c8
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-10-23 16:40:53 +0200

    feat(mail): add converters for GameMail attachments
    * Implement GameMailAttachmentCurrencyConverter for currency attachments.
    * Implement GameMailAttachmentItemConverter for item attachments.
    * Create GameMailAttachmentConverter interface for attachment conversion.
    * Add GameMailAttachmentConverterRegistry for managing converters.

---
+++ lsadf_core/src/main/java/com/lsadf/core/infra/persistence/adapter/game/mail/converter/GameMailAttachmentConverterRegistry.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2024-2025 LSDAF
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lsadf.core.infra.persistence.adapter.game.mail.converter;
+
+import com.lsadf.core.domain.game.mail.GameMailAttachmentType;
+import java.util.HashMap;
+import java.util.Map;
+import org.jspecify.annotations.Nullable;
+
+public class GameMailAttachmentConverterRegistry {
+  private final Map<GameMailAttachmentType, GameMailAttachmentConverter<?>> converters =
+      new HashMap<>();
+
+  public void registerConverter(GameMailAttachmentConverter<?> converter) {
+    converters.put(converter.getAttachmentType(), converter);
+  }
+
+  public @Nullable GameMailAttachmentConverter<?> getConverterByType(GameMailAttachmentType type) {
+    return converters.get(type);
+  }
+}
---
+++ lsadf_core/src/main/java/com/lsadf/core/infra/persistence/adapter/game/mail/converter/impl/GameMailAttachmentCurrencyConverter.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2024-2025 LSDAF
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lsadf.core.infra.persistence.adapter.game.mail.converter.impl;
+
+import static com.lsadf.core.domain.game.mail.GameMailAttachmentType.CURRENCY;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lsadf.core.domain.game.mail.GameMailAttachmentType;
+import com.lsadf.core.domain.game.save.currency.Currency;
+import com.lsadf.core.infra.persistence.adapter.game.mail.converter.GameMailAttachmentConverter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class GameMailAttachmentCurrencyConverter implements GameMailAttachmentConverter<Currency> {
+
+  private final ObjectMapper objectMapper;
+
+  @Override
+  public GameMailAttachmentType getAttachmentType() {
+    return CURRENCY;
+  }
+
+  @Override
+  public String toJson(Currency attachment) throws JsonProcessingException {
+    return objectMapper.writeValueAsString(attachment);
+  }
+
+  @Override
+  public Currency toModel(String json) throws JsonProcessingException {
+    return objectMapper.readValue(json, Currency.class);
+  }
+}
