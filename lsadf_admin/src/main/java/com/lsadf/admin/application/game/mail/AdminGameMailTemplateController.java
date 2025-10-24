24f4dd8d1c72b6e002c74c1711d185e276e871d1
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-10-24 18:31:19 +0200

    feat(mail): implement admin game mail template controller
    * Add AdminGameMailTemplateController interface for managing game mail templates.
    * Implement AdminGameMailTemplateControllerImpl with methods to get all templates, get a template by ID, and delete a template by ID.
    * Ensure proper authentication and response handling for each endpoint.

---
+++ lsadf_admin/src/main/java/com/lsadf/admin/application/game/mail/AdminGameMailTemplateController.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright © 2024-2025 LSDAF
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lsadf.admin.application.game.mail;
+
+import static com.lsadf.admin.application.game.mail.AdminGameMailTemplateController.Constants.ApiPaths.GAME_MAIL_TEMPLATE_ID;
+import static com.lsadf.core.infra.web.config.swagger.SwaggerAuthenticationStrategies.BEARER_AUTHENTICATION;
+import static com.lsadf.core.infra.web.config.swagger.SwaggerAuthenticationStrategies.OAUTH2_AUTHENTICATION;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.lsadf.admin.application.constant.AdminApiPathConstants;
+import com.lsadf.admin.application.constant.AdminSwaggerConstants;
+import com.lsadf.core.infra.web.controller.Controller;
+import com.lsadf.core.infra.web.controller.ParameterConstants;
+import com.lsadf.core.infra.web.dto.response.ApiResponse;
+import com.lsadf.core.infra.web.dto.response.game.mail.GameMailTemplateResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/** */
+@RequestMapping(value = AdminApiPathConstants.ADMIN_GAME_MAIL_TEMPLATE)
+@Tag(name = AdminSwaggerConstants.ADMIN_GAME_MAIL_TEMPLATE_CONTROLLER)
+@SecurityRequirement(name = BEARER_AUTHENTICATION)
+@SecurityRequirement(name = OAUTH2_AUTHENTICATION)
+public interface AdminGameMailTemplateController extends Controller {
+
+  @GetMapping
+  @Operation(summary = "Gets ll the game mail templates")
+  @ApiResponses(
+      value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "401",
+            description = "Unauthorized"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "403",
+            description = "Forbidden"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "200",
+            description = "OK"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error")
+      })
+  ResponseEntity<ApiResponse<List<GameMailTemplateResponse>>> getAllTemplates(
+      @AuthenticationPrincipal Jwt jwt);
+
+  @GetMapping(value = GAME_MAIL_TEMPLATE_ID)
+  @Operation(summary = "Gets a specific game mail template by its ID")
+  @ApiResponses(
+      value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "401",
+            description = "Unauthorized"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "403",
+            description = "Forbidden"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "200",
+            description = "OK"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "404",
+            description = "Not Found"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error")
+      })
+  ResponseEntity<ApiResponse<GameMailTemplateResponse>> getTemplateById(
+      @AuthenticationPrincipal Jwt jwt,
+      @PathVariable(value = ParameterConstants.GAME_MAIL_TEMPLATE_ID) UUID id)
+      throws JsonProcessingException;
+
+  @DeleteMapping(value = GAME_MAIL_TEMPLATE_ID)
+  @Operation(summary = "Deletes a game mail template using its ID")
+  @ApiResponses(
+      value = {
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "401",
+            description = "Unauthorized"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "403",
+            description = "Forbidden"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "200",
+            description = "OK"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "404",
+            description = "Not Found"),
+        @io.swagger.v3.oas.annotations.responses.ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error")
+      })
+  ResponseEntity<ApiResponse<Void>> deleteMailTemplateById(
+      @AuthenticationPrincipal Jwt jwt,
+      @PathVariable(value = ParameterConstants.GAME_MAIL_TEMPLATE_ID) UUID id);
+
+  @NoArgsConstructor(access = AccessLevel.PRIVATE)
+  class Constants {
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static final class ApiPaths {
+      public static final String GAME_MAIL_TEMPLATE_ID = "/{game_mail_template_id}";
+    }
+  }
+}
---
+++ lsadf_admin/src/main/java/com/lsadf/admin/application/game/mail/AdminGameMailTemplateControllerImpl.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright © 2024-2025 LSDAF
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lsadf.admin.application.game.mail;
+
+import static com.lsadf.core.infra.web.dto.response.ResponseUtils.generateResponse;
+import static org.springframework.http.HttpStatus.OK;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.lsadf.core.application.game.mail.GameMailTemplateCommandService;
+import com.lsadf.core.application.game.mail.GameMailTemplateQueryService;
+import com.lsadf.core.infra.web.controller.BaseController;
+import com.lsadf.core.infra.web.dto.response.ApiResponse;
+import com.lsadf.core.infra.web.dto.response.game.mail.GameMailTemplateResponse;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class AdminGameMailTemplateControllerImpl extends BaseController
+    implements AdminGameMailTemplateController {
+
+  private final GameMailTemplateQueryService gameMailTemplateQueryService;
+  private final GameMailTemplateCommandService gameMailTemplateCommandService;
+
+  @Override
+  public ResponseEntity<ApiResponse<List<GameMailTemplateResponse>>> getAllTemplates(Jwt jwt) {
+    validateUser(jwt);
+
+    var results = gameMailTemplateQueryService.getMailTemplates();
+    return generateResponse(OK, results);
+  }
+
+  @Override
+  public ResponseEntity<ApiResponse<GameMailTemplateResponse>> getTemplateById(Jwt jwt, UUID id)
+      throws JsonProcessingException {
+    validateUser(jwt);
+
+    var result = gameMailTemplateQueryService.getMailTemplateById(id);
+    return generateResponse(OK, result);
+  }
+
+  @Override
+  public ResponseEntity<ApiResponse<Void>> deleteMailTemplateById(Jwt jwt, UUID id) {
+    validateUser(jwt);
+
+    gameMailTemplateCommandService.deleteGameMailTemplateById(id);
+    return generateResponse(OK);
+  }
+
+  @Override
+  public Logger getLogger() {
+    return log;
+  }
+}
