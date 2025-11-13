9f10270fead4e1441ac2fc443305267271be9907
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-11-13 13:15:22 +0100

    feat(event): implement event factories for game mail and inventory
    * Add GameMailEventFactory for creating game mail events.
    * Add InventoryEventFactory for creating inventory events.
    * Configure beans for event factories in EventFactoryConfiguration.
    * Include unit tests for event factories to ensure correct behavior.

---
+++ lsadf_core/src/main/java/com/lsadf/core/infra/event/config/EventFactoryConfiguration.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2024-2025 LSDAF
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lsadf.core.infra.event.config;
+
+import com.lsadf.core.application.clock.ClockService;
+import com.lsadf.core.infra.event.factory.game.inventory.InventoryEventFactory;
+import com.lsadf.core.infra.event.factory.game.mail.GameMailEventFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EventFactoryConfiguration {
+  @Bean
+  public InventoryEventFactory inventoryEventFactory(ClockService clockService) {
+    return new InventoryEventFactory(clockService);
+  }
+
+  @Bean
+  public GameMailEventFactory gameMailEventFactory(ClockService clockService) {
+    return new GameMailEventFactory(clockService);
+  }
+}
