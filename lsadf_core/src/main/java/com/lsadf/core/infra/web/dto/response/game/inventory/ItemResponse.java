45aec1872faa1f23145cf30571c0be06897992ce
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-11-14 12:49:35 +0100

    refactor(response): update ItemResponse fields for clarity
    * Renamed fields in ItemResponse for better understanding
    * Updated mapping in ItemResponseMapper to reflect new field names
    * Adjusted usage in BddInventoryWhenStepDefinitions for consistency

--- lsadf_core/src/main/java/com/lsadf/core/infra/web/dto/response/game/inventory/ItemResponse.java
+++ lsadf_core/src/main/java/com/lsadf/core/infra/web/dto/response/game/inventory/ItemResponse.java
@@ -16,13 +16,9 @@
 
 package com.lsadf.core.infra.web.dto.response.game.inventory;
 
-import static com.lsadf.core.infra.web.JsonAttributes.*;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.lsadf.core.domain.game.inventory.ItemRarity;
 import com.lsadf.core.domain.game.inventory.ItemType;
-import com.lsadf.core.infra.web.JsonAttributes;
 import com.lsadf.core.infra.web.dto.common.game.inventory.ItemStatDto;
 import com.lsadf.core.infra.web.dto.response.Response;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -33,25 +29,16 @@ import lombok.Builder;
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ItemResponse(
-    @JsonProperty(value = JsonAttributes.ID)
-        @Schema(description = "User Id", example = "7d9f92ce-3c8e-4695-9df7-ce10c0bbaaeb")
-        String id,
-    @JsonProperty(value = CLIENT_ID)
-        @Schema(
+    @Schema(description = "User Id", example = "7d9f92ce-3c8e-4695-9df7-ce10c0bbaaeb") String id,
+    @Schema(
             description = "Client generated id, concatenation of inventory id and item id",
             example = "36f27c2a-06e8-4bdb-bf59-56999116f5ef__11111111-1111-1111-1111-111111111111")
         String clientId,
-    @JsonProperty(value = BLUEPRINT_ID)
-        @Schema(description = "Blueprint id", example = "blueprint_id")
-        String blueprintId,
-    @JsonProperty(value = TYPE) @Schema(description = "Item type", example = "BOOTS")
-        ItemType itemType,
-    @JsonProperty(value = RARITY) @Schema(description = "Item rarity", example = "LEGENDARY")
-        ItemRarity itemRarity,
-    @JsonProperty(value = IS_EQUIPPED) @Schema(description = "Is Equipped", example = "true")
-        Boolean isEquipped,
-    @JsonProperty(value = LEVEL) @Schema(description = "Item level", example = "20") Integer level,
-    @JsonProperty(value = MAIN_STAT) @Schema(description = "Main item stat") ItemStatDto mainStat,
-    @JsonProperty(value = ADDITIONAL_STATS) @Schema(description = "Additional item stat list")
-        List<ItemStatDto> additionalStats)
+    @Schema(description = "Blueprint id", example = "blueprint_id") String blueprintId,
+    @Schema(description = "Item type", example = "BOOTS") ItemType type,
+    @Schema(description = "Item rarity", example = "LEGENDARY") ItemRarity rarity,
+    @Schema(description = "Is Equipped", example = "true") Boolean isEquipped,
+    @Schema(description = "Item level", example = "20") Integer level,
+    @Schema(description = "Main item stat") ItemStatDto mainStat,
+    @Schema(description = "Additional item stat list") List<ItemStatDto> additionalStats)
     implements Response {}
