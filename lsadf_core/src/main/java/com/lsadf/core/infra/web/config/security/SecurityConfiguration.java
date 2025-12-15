9884241482d67492c227ca417a1ef859dcb6ff29
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-12-15 22:15:03 +0100

    refactor(security): remove method security annotation

--- lsadf_core/src/main/java/com/lsadf/core/infra/web/config/security/SecurityConfiguration.java
+++ lsadf_core/src/main/java/com/lsadf/core/infra/web/config/security/SecurityConfiguration.java
@@ -26,7 +26,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -46,7 +45,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Import({OAuth2Properties.class})
 @EnableWebSecurity
 @RequiredArgsConstructor
-@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true, proxyTargetClass = true)
 public class SecurityConfiguration implements WebMvcConfigurer {
 
   private RequestLoggerInterceptor requestLoggerInterceptor;
