d4354a4e5850d16d28808f8112a6c02d290dae39
Louis SANTUCCI <louissantucci1998@gmail.com>
2026-01-15 16:28:59 +0100

    refactor(serializer): update serializers to use tools.jackson package
    * Changed DateToLongSerializer to extend ValueSerializer
    * Changed LongToDateSerializer to extend ValueDeserializer
    * Updated import statements to use tools.jackson classes

--- lsadf_core/src/main/java/com/lsadf/core/infra/serializer/LongToDateSerializer.java
+++ lsadf_core/src/main/java/com/lsadf/core/infra/serializer/LongToDateSerializer.java
@@ -15,16 +15,14 @@
  */
 package com.lsadf.core.infra.serializer;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import java.io.IOException;
 import java.util.Date;
+import tools.jackson.core.JsonParser;
+import tools.jackson.databind.DeserializationContext;
+import tools.jackson.databind.ValueDeserializer;
 
-public class LongToDateSerializer extends JsonDeserializer<Date> {
+public class LongToDateSerializer extends ValueDeserializer<Date> {
   @Override
-  public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
-      throws IOException {
+  public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) {
     return new Date(jsonParser.getLongValue());
   }
 }
